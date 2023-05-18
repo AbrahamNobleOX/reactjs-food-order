5a4b600e732821bd46728356db69141b70e0b207
Akintola Abraham <akintolaabrahamidowu@gmail.com>
2023-05-18 11:58:11 +0100

    main restored

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import CartProvider from "./store/CartProvider";
-
 import Cart from "./components/Cart/Cart";
 
 function App() {
@@ -17,8 +18,8 @@ function App() {
 
   return (
     <CartProvider>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
